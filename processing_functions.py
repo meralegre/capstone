2cbd06f215dc8066fb04e04cacbec3c640bfd829
meralegre <maria01astur@gmail.com>
2024-04-10 13:41:42 +0200

    added preprocessing process

--- processing_functions.py
+++ processing_functions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def convert_to_numeric(obj):
     """
     Convert object to an int, or float, or returns the object (string)
@@ -11,3 +13,25 @@ def convert_to_numeric(obj):
             return float(obj)
         except ValueError:
             return obj
+
+def nan_percentage(df: pd.DataFrame):
+    """
+    Returns the percentage of nan values per column in a dataframe
+    """
+    for col in df.columns:
+        print("NaN % {}: {:0.2f}%".format(col, df[col].isna().mean() * 100))
+        
+
+def rename_columns(df, rename_suffix):
+    """
+    Modifies column names of a DataFrame based on specified renaming rules.
+    Each rule in the `rename_suffix` specifies a suffix to look for and a 
+    suffix to append.
+    """
+    new_columns = df.columns.tolist()
+    for i, col in enumerate(df.columns):
+        for suffix_change, suffix_add in rename_suffix:
+            if col.endswith(suffix_change):
+                new_columns[i] = col.replace(suffix_change, '') + suffix_add
+                break  # Assumes only one rule applies per column, goes to the next column after a replacement
+    return df.rename(columns=dict(zip(df.columns, new_columns)))
